c12fdf02ce2631207c3fb9180483fb2f5709bce0
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-03-26 14:53:54 +0530

    long challenge march22 2

--- qn4.cpp
+++ qn4.cpp
@@ -1,37 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-///I hope I will get uprating and don't make mistakes
-///I will never stop programming
-///sqrt(-1) Love C++
-///Please don't hack me
-///Gourav Yo
-///Think different approaches (bs,dp,greedy,graphs,shortest paths,mst)
-///Stay Calm
-///Look for special cases
-///Beware of overflow and array bounds
-///Think the problem backwards
-///Training
+
 int main()
 {
+
     int t;
     cin >> t;
     while (t--)
 
     {
-        long long int n;
-        cin >> n;
-        if (n < 6)
-        {
-            cout << "0" << endl;
-        }
-
-        else if (n == 6)
-        {
-            cout << "1" << endl;
-        }
-        else 
-        {
-            cout << (n+1) / 7 << endl;
-        }
+       int a;
+       cin>>a;
+       cout<<-(a+1)<<endl;
     }
 }
