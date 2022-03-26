ec6f9958498b3a5998bf68f6d52f1e3b750e104b
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-03-26 14:54:46 +0530

    more added

---
+++ qn1Prac.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int c, x, y;
+        cin >> c>> x>>y;
+        cout<<(c-x)*y<<endl;
+        //     string s;
+        //     cin >> s;
+        //     int countz = 0;
+        //     int counto = 0;
+        //     for (int i = 0; i < n; i++)
+
+        //         if (s[i] == '1')
+
+        //             counto++;
+
+        //         else
+        //             countz++;
+
+        //     while (countz)
+        //     {
+        //         cout << "0";
+        //         countz--;
+        //     }
+        //     while (counto)
+        //     {
+        //         cout << "1";
+        //         counto--;
+        //     }
+        //     cout << endl;
+        // }
+
+
+        // your code goes here
+    }
+            return 0;
+}
